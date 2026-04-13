8b22129ec25dfe237cff6001fb4250a93a8a7c46
djalabz <djalabz@gmail.com>
2026-04-13 09:19:46 +0200

    :tada: first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+// Todo :
+// Une première page un peu personnalisée en Flutter
+// Dans votre scaffold vous aurez :
+//  - Une appbar personnalisée -> Changer la couleur de fond, une autre police, et au moins 2 icones de votre choix
+//  - Pour le body -> Un Container avec dedans un bouton (qui servira d'entrer dans l'application)
+//  et un message associé de type Bienvenu sur monApp
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("MyApp")),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('This is a snackbar')),
+              );
+            },
+            icon: const Icon(Icons.menu),
+          ),
+        ],
+      ),
+      body: Container(
+        color: Colors.amber[50],
+        child: Row(
+          children: [ElevatedButton(onPressed: () {}, child: Text("ok"))],
+        ),
+      ),
+    );
+  }
+}
